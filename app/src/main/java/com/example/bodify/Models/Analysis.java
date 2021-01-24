e07b3b0129a3e43e25bebe52f52215369e98c793
AjaideepSingh <c17720695@mytudublin.ie>
2021-01-24 15:58:35 +0000

    almost finished background service for Analysis screen

--- app/src/main/java/com/example/bodify/Models/Analysis.java
+++ app/src/main/java/com/example/bodify/Models/Analysis.java
@@ -1,48 +1,58 @@
 package com.example.bodify.Models;
 
 public class Analysis {
-    private double calories,fats,carbohydrates,proteins;
+    private int calories,fats,carbohydrates,proteins;
+    private String userID;
 
     public Analysis() {
 
     }
 
-    public double getCalories() {
+    public int getCalories() {
         return calories;
     }
 
-    public void setCalories(double calories) {
+    public void setCalories(int calories) {
         this.calories = calories;
     }
 
-    public double getFats() {
+    public int getFats() {
         return fats;
     }
 
-    public void setFats(double fats) {
+    public void setFats(int fats) {
         this.fats = fats;
     }
 
-    public double getCarbohydrates() {
+    public int getCarbohydrates() {
         return carbohydrates;
     }
 
-    public void setCarbohydrates(double carbohydrates) {
+    public void setCarbohydrates(int carbohydrates) {
         this.carbohydrates = carbohydrates;
     }
 
-    public double getProteins() {
+    public int getProteins() {
         return proteins;
     }
 
-    public void setProteins(double proteins) {
+    public void setProteins(int proteins) {
         this.proteins = proteins;
     }
 
-    public Analysis(double calories, double fats, double carbohydrates, double proteins) {
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public Analysis(int calories, int fats, int carbohydrates, int proteins, String userID) {
         this.calories = calories;
         this.fats = fats;
         this.carbohydrates = carbohydrates;
         this.proteins = proteins;
+        this.userID = userID;
     }
 }
--- app/src/main/java/com/example/bodify/MyService.java
+++ app/src/main/java/com/example/bodify/MyService.java
@@ -5,7 +5,9 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
+
 import androidx.annotation.NonNull;
+
 import com.example.bodify.Models.Analysis;
 import com.example.bodify.Models.Meal;
 import com.google.firebase.auth.FirebaseAuth;
@@ -14,8 +16,11 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.lang.reflect.Array;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 
 public class MyService extends Service {
@@ -24,6 +29,8 @@ public class MyService extends Service {
     private final SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
     private final FirebaseAuth mAuth = FirebaseAuth.getInstance();
     private final String userID = mAuth.getUid();
+    private int calories,fats,proteins,carbohydrates;
+    private final ArrayList<String> daysInDB;
 
     @Override
     public IBinder onBind(Intent arg0) {
@@ -43,49 +50,80 @@ public class MyService extends Service {
     }
 
     public MyService() {
-        Log.i("hey", "Hello");
-        final ArrayList<String> daysOfWeek = new ArrayList<>();
-        daysOfWeek.add("Monday");
-        daysOfWeek.add("Tuesday");
-        daysOfWeek.add("Wednesday");
-        daysOfWeek.add("Thursday");
-        daysOfWeek.add("Friday");
-        daysOfWeek.add("Saturday");
-        daysOfWeek.add("Sunday");
+//        final ArrayList<String> daysOfWeek = new ArrayList<>();
+//        daysOfWeek.add("Monday");
+//        daysOfWeek.add("Tuesday");
+//        daysOfWeek.add("Wednesday");
+//        daysOfWeek.add("Thursday");
+//        daysOfWeek.add("Friday");
+//        daysOfWeek.add("Saturday");
+//        daysOfWeek.add("Sunday");
         Date date = new Date();
         String currentDate = formatter.format(date);
         //minus current date from when it was created and if the difference is 7
         //error with for loop
-        for (int i = 0; i < daysOfWeek.size(); i++) {
-            DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("DayOfWeek").child(daysOfWeek.get(i));
-            databaseReference.addValueEventListener(new ValueEventListener() {
-                int calories, fats, proteins, carbohydrates;
+        daysInDB = new ArrayList<>();
+        //lets go through the days of week database and see what days the food has been logged
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("DayOfWeek");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot userSnapshot : snapshot.getChildren()) {
+                    daysInDB.add(userSnapshot.getKey());
+                    Log.i("", "" + daysInDB);
+                }
+                test(daysInDB);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        Log.i("days", "" + daysInDB);
+        //I need to read the DB again but loop i
+
+        //now what i can do is I can for loop the daysInDB and then append I to the next reading of the database
+        //I need to think of logic to make sure a full week has gone by with the analysis
+        //I think I have figured it out
+        //lets say we have an arraylist of meal objects lets access the dates of all of them
+        //lets then get the smallest value and the largest value
+        //if the difference is 7 then a full week has gone by, but I need to think of the OR logic what if a user just isnt the app for the full week how will I know if a full week has gone by??
+        //if(largest - smallest == 7 OR .......) {
+        // One week has gone by
+        //create analysis object
+        // }
+
+    }
+
+//    public void createAnalysis(Analysis analysis) {
+
+//    }
+
+
+    public void test(ArrayList<String> daysInDB) {
+        final ArrayList<String> dates = new ArrayList<>();
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("DayOfWeek");
+        for (int i = 0; i < daysInDB.size(); i++) {
+            Log.i("i", "" + daysInDB.get(i));
+            databaseReference.child(daysInDB.get(i)).addValueEventListener(new ValueEventListener() {
+
 
                 @Override
                 public void onDataChange(@NonNull DataSnapshot snapshot) {
                     for (DataSnapshot userSnapshot : snapshot.getChildren()) {
                         Meal meal = userSnapshot.getValue(Meal.class);
                         assert meal != null;
-                        if (meal.getUserID().equals(userID)) {
-                            String mealCreationDate = meal.getDate();
-                            String cd = currentDate.substring(0, 2);
-                            String mcd = mealCreationDate.substring(0, 2);
-                            int intCd = Integer.parseInt(cd);
-                            int intMcd = Integer.parseInt(mcd);
-                            if (intCd - intMcd == 7) {
-                                //One week has gone by if the result is == 7
-                                //Now I need to remove this
-                                calories += meal.getCalories() * meal.getNumberOfServings();
-                                fats += meal.getItemTotalFat() * meal.getNumberOfServings();
-                                proteins += meal.getItemProtein() * meal.getNumberOfServings();
-                                carbohydrates += meal.getItemTotalCarbohydrates() * meal.getNumberOfServings();
-                            } else {
-                                Log.i("D", "7 days have not passed since creation");
-                            }
+                        if (meal.getUserID().equalsIgnoreCase(userID)) {
+                            dates.add(meal.getDate());
+                            calories += meal.getCalories() * meal.getNumberOfServings();
+                            fats += meal.getItemTotalFat() * meal.getNumberOfServings();
+                            carbohydrates += meal.getItemTotalCarbohydrates() * meal.getNumberOfServings();
+                            proteins += meal.getItemProtein() * meal.getNumberOfServings();
                         }
                     }
-                    Analysis analysis = new Analysis(calories, fats, carbohydrates, proteins);
-                    createAnalysis(analysis);
+                    //do division operator here
+                    dates(dates,calories / 7,fats / 7,carbohydrates / 7,proteins / 7);
                 }
 
                 @Override
@@ -96,13 +134,28 @@ public class MyService extends Service {
         }
     }
 
-    public void createAnalysis(Analysis analysis) {
-        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("Analysis");
-        databaseReference.push().setValue(analysis).addOnCompleteListener(task -> {
-            if (task.isSuccessful()) {
-                Log.i("A","Successfully saved");
-            }
-        }).addOnFailureListener(e -> Log.i("B","Error occurred: " + e.getMessage()));
+    public void dates(ArrayList<String> dates,int calories,int fats,int carbohydrates,int proteins) {
+        //need to find averages
+        ArrayList<Integer> intDates = new ArrayList<>();
+        for(int i = 0; i < dates.size(); i++) {
+            intDates.add(Integer.parseInt(dates.get(i).substring(0,2)));
+        }
+        Log.i("newDates","" + intDates);
+        //if the if statement gets executed that means one week has gone by now I just need to find the OR clause to because the user might not use the app much
+        int b = Collections.max(intDates);
+        int s = Collections.min(intDates);
+        Log.i("D","" + (b - s));
+        if(Collections.max(intDates) - Collections.min(intDates) == 6) {
+            Analysis analysis = new Analysis(calories,fats,carbohydrates,proteins,userID);
+            DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("Analysis");
+            databaseReference.push().setValue(analysis).addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    Log.i("A", "Successfully saved");
+                }
+            }).addOnFailureListener(e -> Log.i("B", "Error occurred: " + e.getMessage()));
+        } else {
+            //one week has not passed yet
+            Log.i("error","7 days have not passed yet");
+        }
     }
-
 }
